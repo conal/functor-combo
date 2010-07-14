01a816197064743b8378087ec95cc60877382dd3
Conal Elliott <conal@conal.net>
2010-07-14 11:31:09 -0700

    Functor: Functor instances
    Ignore-this: 76ac59073c331918ebe7ebbbf904151a
    
    darcs-hash:20100714183109-fb517-03893893b3182c93c137edf2e5d302fc984dfdd0

--- src/FunctorCombo/Functor.hs
+++ src/FunctorCombo/Functor.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeOperators, EmptyDataDecls #-}
+{-# LANGUAGE TypeOperators, EmptyDataDecls, StandaloneDeriving #-}
 {-# OPTIONS_GHC -Wall #-}
 ----------------------------------------------------------------------
 -- |
@@ -14,13 +14,13 @@
 
 module FunctorCombo.Functor
   (
-    Consta,Void,Unit,Id(..),inId,(:+:)(..),(:*:)(..),(:.)(..),inO,(~>)
+    Const,Void,Unit,Id(..),inId,inId2,(:+:)(..),(:*:)(..),(:.)(..),inO,inO2,(~>)
   ) where
 
 
-import Control.Applicative (Const)
+import Control.Applicative (Applicative(..),Const(..))
 
-import Control.Compose (Id(..),inId,(:.)(..), inO,(~>))
+import Control.Compose (Id(..),inId,inId2,(:.)(..), inO,inO2,(~>))
 
 
 -- infixl 9 :.
@@ -51,3 +51,51 @@ data (f :+: g) a = L (f a) | R (g a)
 -- From Control.Compose:
 -- 
 --   newtype (g :. f) a = O { unO :: g (f a) }
+
+
+{--------------------------------------------------------------------
+    Functor and Applicative instances for generic constructors
+--------------------------------------------------------------------}
+
+instance Functor Void where
+  fmap _ = error "Void fmap: no void value"  -- so ghc won't complain
+
+-- instance Functor Id where
+--   fmap h (Id a) = Id (h a)
+
+-- deriving instance Functor Id
+
+-- instance (Functor f, Functor g) => Functor (f :+: g) where
+--   fmap h (L fa) = L (fmap h fa)
+--   fmap h (R ga) = R (fmap h ga)
+
+-- i.e.,
+-- 
+--     fmap h . L  ==  L . fmap h
+--     fmap h . R  ==  R . fmap h
+
+deriving instance (Functor f, Functor g) => Functor (f :+: g)
+
+-- instance (Functor f, Functor g) => Functor (f :*: g) where
+--   fmap h (fa :*: ga) = fmap h fa :*: fmap h ga
+
+-- Or:
+deriving instance (Functor f, Functor g) => Functor (f :*: g)
+
+-- TODO: Verify that the deriving instances are equivalent to the explicit versions.
+
+-- What about Applicative instances?  I think Void could implement (<*>)
+-- but not pure.  Hm.  Id and (:*:) are easy, while (:+:) is problematic.
+
+-- instance Applicative Id where
+--   pure a = Id a
+--   Id f <*> Id x = Id (f x)
+
+-- instance Applicative Id where
+--   pure  = Id
+--   (<*>) = inId2 ($)
+
+instance (Applicative f, Applicative g) => Applicative (f :*: g) where
+  pure a = pure a :*: pure a
+  (f :*: g) <*> (a :*: b) = (f <*> a) :*: (g <*> b)
+
